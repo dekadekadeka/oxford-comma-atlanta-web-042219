52683770adba6b27f3892413900f1c0e76eae311
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 19:50:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 last = array.pop
 if array.size == 1
   return last
-elsif array.size <= 2
+elsif array.size == 2
   array.join(" and ")
 elsif array.size >= 3
   array << ("and " + last)
