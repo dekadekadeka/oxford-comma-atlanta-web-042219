53a33ffe33af957fd53a35af7068d5c4962e005c
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-02 19:51:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,6 @@
 def oxford_comma(array)
 last = array.pop
-if array.size == 1
-  return last
-elsif array.size == 2
-  array.join(" and ")
-elsif array.size >= 3
-  array << ("and " + last)
-  array.join(", ")
+#array << ("and " + last)
+array.join(", ")
 end
 end
